42f7e044c33843c2937d0cbfa571e783b5ee5fa3
Camila-Negron <carla.negron@ucb.edu.bo>
2022-11-16 14:33:20 -0400

    version5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PromocionesComponent } from './promociones/promociones.component';
+import { NovedadesComponent } from './novedades/novedades.component';
 
-const routes: Routes = [];
+const router: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+
+  {
+    path: 'promociones',
+    component: PromocionesComponent
+  },
+
+  {
+    path: 'novedades',
+    component: NovedadesComponent
+  }
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(router)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule,Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { PromocionesComponent } from './promociones/promociones.component';
+import { NovedadesComponent } from './novedades/novedades.component';
+
+
+const router: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+
+  {
+    path: 'promociones',
+    component: PromocionesComponent
+  },
+
+  {
+    path: 'novedades',
+    component: NovedadesComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
+    MenuComponent,
     HomeComponent,
-    MenuComponent
+    PromocionesComponent,
+    NovedadesComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(router)
   ],
   providers: [],
   bootstrap: [AppComponent]
